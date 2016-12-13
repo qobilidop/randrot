bb971eddc73ff40fe257b114fa538749b6d189b3
Bili Dong <qobilidop@gmail.com>
2016-12-12 17:41:52 -0800

    Improve readability

--- tests/test_randrot.py
+++ tests/test_randrot.py
@@ -4,7 +4,9 @@ import randrot
 
 
 def test_orthogonality():
+
+    def orthogonal(m):
+        return np.allclose(m.T * m, np.eye(dim))
+
     for dim in [2, 3]:
-        m = randrot.generate(dim)
-        orthogonal = np.allclose(m.T * m, np.eye(dim))
-        assert orthogonal is True
+        assert orthogonal(randrot.generate(dim))
