687f0a8fdc4b208e38aa36ffc4be1e2f837fcf34
Bili Dong <qobilidop@gmail.com>
2016-12-12 17:25:18 -0800

    Improve docstrings

--- randrot/__init__.py
+++ randrot/__init__.py
@@ -1,25 +1,44 @@
 """Random rotation matrix generators."""
+
 import numpy as np
 
 
-def generate(dim: int) -> np.matrix:
-    """Generate a random rotation matrix."""
+def generate(dim):
+    """Generate a random rotation matrix.
+
+    Args:
+        dim (int): The dimension of the matrix
+
+    Returns:
+        np.matrix: A rotation matrix
+
+    """
     if dim == 2:
         return generate_2d()
     elif dim == 3:
         return generate_3d()
 
 
-def generate_2d() -> np.matrix:
-    """Generate a 2D random rotation matrix."""
+def generate_2d():
+    """Generate a 2D random rotation matrix.
+
+    Returns:
+        np.matrix: A 2D rotation matrix
+
+    """
     x = np.random.random()
     M = np.matrix([[np.cos(2 * np.pi * x), -np.sin(2 * np.pi * x)],
                    [np.sin(2 * np.pi * x), np.cos(2 * np.pi * x)]])
     return M
 
 
-def generate_3d() -> np.matrix:
-    """Generate a 3D random rotation matrix."""
+def generate_3d():
+    """Generate a 3D random rotation matrix.
+
+    Returns:
+        np.matrix: A 3D rotation matrix
+
+    """
     x1, x2, x3 = np.random.rand(3)
     R = np.matrix([[np.cos(2 * np.pi * x1), np.sin(2 * np.pi * x1), 0],
                    [-np.sin(2 * np.pi * x1), np.cos(2 * np.pi * x1), 0],
